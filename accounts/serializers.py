e597144254c917715a0ed8e8b42a8d3d3cce7dff
Fernando Julca <fernandojulcap@gmail.com>
2025-09-18 09:10:23 -0500

    accounts echo

---
+++ accounts/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+    
+    def create(self, validated_data):
+        # Crear usuario con contraseña hasheada
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+        
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError('Credenciales inválidas')
+            if not user.is_active:
+                raise serializers.ValidationError('Usuario inactivo')
+            data['user'] = user
+            return data
+        else:
+            raise serializers.ValidationError('Debe proporcionar username y password')
