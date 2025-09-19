87c67d9acedeb9e197a81fa53715f1c34af68b06
Fernando Julca <fernandojulcap@gmail.com>
2025-09-19 13:03:04 -0500

    actualizando

--- Horarios/views.py
+++ Horarios/views.py
@@ -4,13 +4,15 @@ from rest_framework.decorators import action
 from .models import HorarioRecuperacion
 from .serializers import HorarioRecuperacionSerializer
 from .utils import export_horarios_to_excel
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action, permission_classes
 
 class HorarioRecuperacionViewSet(viewsets.ModelViewSet):
     queryset = HorarioRecuperacion.objects.all()
     serializer_class = HorarioRecuperacionSerializer
 
     @action(detail=False, methods=['get'], url_path='exportar-excel')
+    @permission_classes([IsAuthenticated])
     def exportar_excel(self, request):
         queryset = self.get_queryset()
         excel_file = export_horarios_to_excel(queryset)
