bb81989382ab3ec85f6f9efb42a59012d9111801
Fernando Julca <fernandojulcap@gmail.com>
2025-09-19 13:19:44 -0500

    actualizando

--- Habilidades/views.py
+++ Habilidades/views.py
@@ -2,8 +2,15 @@ from rest_framework import viewsets
 from django.http import HttpResponse
 from rest_framework.decorators import action
 from .models import Habilidad, NivelHabilidad
-from .serializers import HabilidadSerializer, NivelHabilidadSerializer
+from .serializers import HabilidadSimpleSerializer, NivelHabilidadSerializer, PracticantePuntajeSerializer
+from Practicantes.models import Practicante
+from django.shortcuts import render
 from .utils import export_niveles_habilidad_to_excel
+
+class PracticantePuntajeViewSet(viewsets.ModelViewSet):
+    queryset = Practicante.objects.all()
+    serializer_class = PracticantePuntajeSerializer
+
 
 class HabilidadViewSet(viewsets.ModelViewSet):
     queryset = Habilidad.objects.all()
@@ -14,6 +21,8 @@ class NivelHabilidadViewSet(viewsets.ModelViewSet):
     queryset = NivelHabilidad.objects.all()
     serializer_class = NivelHabilidadSerializer
 
+def habilidades_template(request):
+    return render(request, 'habilidades.html')
     @action(detail=False, methods=['get'], url_path='exportar-excel')
     def exportar_excel(self, request):
         queryset = self.get_queryset()
